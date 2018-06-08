50fcf1b513ca12a47ff3b428838f7f3e60ec045a
Lintang Wisesa <lintangwisesa@ymail.com>
2018-06-08 10:37:36 +0700

    React Firebase Database

---
+++ 18a_React_Firebase_Database.js
@@ -0,0 +1,86 @@
+// import React, { Component } from 'react';
+// import fire from './fire';
+
+// class App extends Component {
+
+//   constructor(){
+//     super();
+//     this.state = {pemainBola:[]}
+//   }
+
+//   componentWillMount(){
+//     let listPemain = fire.database().ref('dataku').orderByKey().limitToLast(100);
+//     listPemain.on('child_added', snapshot => {
+//       let list = { nama: snapshot.val(), id: snapshot.key };
+//       this.setState(
+//         {pemainBola:[list].concat(this.state.pemainBola)}
+//       );
+//     })
+//   }
+
+//   kirimData(x){
+//     x.preventDefault();
+//     fire.database().ref('dataku').push(this.refs.nama.value);
+//   }
+
+//   render(){
+//     return(
+//       <div>
+//         <center>
+//         <h3>React ♥ Firebase Database</h3>
+
+//         <form onSubmit={this.kirimData.bind(this)}>
+//           <input type="text" ref="nama" />
+//           <input type="submit"/>
+//         </form>
+
+//       </center>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App;
+
+
+
+import React, { Component } from 'react';
+import fire from './fire';
+class App extends Component {
+ constructor(props) {
+  super(props);
+  this.state = { messages: [] };
+}
+
+componentWillMount(){
+ let messagesRef = fire.database().ref('data_lin').orderByKey().limitToLast(100);
+ messagesRef.on('child_added', snapshot => {
+ let message = { text: snapshot.val(), id: snapshot.key };
+ this.setState({ messages: [message].concat(this.state.messages)
+ });
+})
+}
+addMessage(e){
+  e.preventDefault();
+  fire.database().ref('data_lin').push( this.inputEl.value );
+  this.inputEl.value = '';
+ }
+ 
+ render() {
+  return (
+  <div>
+    <h3>React ♥ Firebase Database</h3>
+   <form onSubmit={this.addMessage.bind(this)}>
+    <input type="text" ref={ el => this.inputEl = el }/>
+    <input type="submit"/>
+    <ul>
+     {
+      this.state.messages.map( message => <li
+      key={message.id}>{message.text}</li> )
+     }
+    </ul>
+   </form>
+   </div>
+ );}}
+ export default App;
+ 
